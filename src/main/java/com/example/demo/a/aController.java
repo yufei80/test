5e620cf0df429130fb80c3a20ff09eefccd87f70
Wyf <1561294236@qq.com>
2022-06-05 13:54:09 +0800

    dev1第二次提交

--- src/main/java/com/example/demo/a/aController.java
+++ src/main/java/com/example/demo/a/aController.java
@@ -3,5 +3,6 @@ package com.example.demo.a;
 public class aController {
     public static void main(String[] args) {
         System.out.println("aaa");
+        System.out.println("222");
     }
 }
